7761e5775da917cc046b58655767143e6c803879
Danting Huang <serena.huangdanting@gmail.com>
2022-03-08 11:23:27 -0500

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
+  if (this.contains(item)) {
+    var index = this._storage.indexOf(item);
+    this._storage.splice(index, 1);
+
+  }
 };
 
 /*
