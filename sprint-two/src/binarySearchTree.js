2656729e90cf14b97a95057a9115235464f5cd3d
Danting Huang <serena.huangdanting@gmail.com>
2022-03-10 00:02:25 -0500

    Complete Binary Search Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,80 @@
 var BinarySearchTree = function(value) {
+
+  var tree = {};
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+  _.extend(tree, BSTMethods);
+  return tree;
+
+};
+
+var BSTMethods = {};
+
+BSTMethods.insert = function(value) {
+  var treeNode = BinarySearchTree(value);
+
+  // if insert value is less than root value,
+  // it will be inserted in the left side of the BST;
+  if (this.value > value) {
+    //check left branch
+    if (!this.left) {
+      this.left = treeNode;
+    } else {
+      this.left.insert(value);
+    }
+  }
+  // if insert value is larger than root value,
+  // it will be inserted in the right side of the BST;
+  if (this.value < value) {
+    //check right branch
+    if (!this.right) {
+      this.right = treeNode;
+    } else {
+      this.right.insert(value);
+    }
+  }
+
+};
+
+
+BSTMethods.contains = function(value) {
+  //check root
+  if (this.value === value) {
+    return true;
+  }
+  if (value < this.value) {
+    if (!this.left) {
+      return false;
+    }
+    return this.left.contains(value);
+  } else {
+    if (!this.right) {
+      return false;
+    }
+    return this.right.contains(value);
+  }
+
+};
+
+BSTMethods.depthFirstLog = function(callback, currNode) {
+  currNode = currNode || this;
+  callback(currNode.value);
+  if (currNode.left) {
+    currNode.depthFirstLog(callback, currNode.left);
+
+  }
+  if (currNode.right) {
+    currNode.depthFirstLog(callback, currNode.right);
+  }
+
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+insert:  O(logn);
+contains: O();
+depthFirstLog: O();
+
  */
